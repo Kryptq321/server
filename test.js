d14683ca5b715ed56c6aecb798a087c2d93339de
Madan301 <111639304+Madan301@users.noreply.github.com>
2022-12-09 03:07:23 +0530

    commit server

---
+++ test.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+const axios = require("axios");
+app.use(cors());
+const server = http.createServer(app);
+const io = new Server(server, {
+	cors: { origin: "*" },
+});
+
+// function startTimer() {
+// 	timer = setInterval(function () {
+// 		axios.get("http://localhost:4444/encrypt").then((res) => {
+// 			key = res.data;
+// 			console.log(key);
+// 		}).catch((err) => {
+// 			console.log(err);
+// 		});
+// 	}, 300000);
+// }
+// startTimer();
+
+
+io.on("connection", (socket) => {
+	const id = socket.handshake.query.id;
+	socket.join(id);
+	console.log(`${id} connected`);
+
+
+	socket.on("share-key", (data) => {
+		socket.to(id).emit("share-key", key);
+	});
+
+	socket.on("send-message", (data) => {
+		console.log(data);
+		let text = data.text;
+		data.recipients.forEach((recipient) => {
+			const newRecipients = data.recipients.filter((r) => r !== recipient);
+			newRecipients.push(id);
+			socket.broadcast.to(recipient).emit("recive-message", {
+				reciver: recipient,
+				recipients: newRecipients,
+				sender: id,
+				text: text,
+			});
+		});
+	});
+});
+
+server.listen(process.env.PORT || 5000, () => {
+	console.log("server up and running ");
+});
